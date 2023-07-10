ebab5d9affcf7ea126f4c20251ade1a85d349475
Kim Alfred <kimalfred@Kims-MacBook-Pro.local>
2023-07-09 21:06:24 -0400

    new

---
+++ src/assets/images/index.js
@@ -0,0 +1,17 @@
+import Weird from './weird.png'
+import Profile from './profile.jpg'
+import Smile from './smile.png'
+import Normal from './normal.jpg'
+// import FlixxitLogo from './netflix.png'
+import LoginBackground from './landingPage.jpg'
+
+let FlixxitLogo = '<h1>Flixxit</h1>'
+
+export {
+    Weird, 
+    Profile, 
+    Smile, 
+    Normal,
+    FlixxitLogo, 
+    LoginBackground
+} 
